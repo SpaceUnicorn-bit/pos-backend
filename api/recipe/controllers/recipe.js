aaae49679346a9593c2edde64d4a84a7b859e99f
SpaceUnicorn-bit <breinersalas14@gmail.com>
2021-04-13 21:40:37 -0600

    query getjoinbilling

--- api/recipe/controllers/recipe.js
+++ api/recipe/controllers/recipe.js
@@ -5,4 +5,16 @@
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+    async getJoinRecipe(ctx) {
+        const knex = strapi.connections.default;
+        const resizes = await knex('recipes_sizes').where({
+            recipe: ctx.params.id,
+        })
+        .join('recipes', 'recipes_sizes.recipe', 'recipes.id')
+        .join('sizes', 'recipes_sizes.size', 'sizes.id')
+        .select('recipes_sizes.prices', 'recipes.name')
+        .select('recipes_sizes.id', 'sizes.size')
+        return resizes;
+    },
+};
